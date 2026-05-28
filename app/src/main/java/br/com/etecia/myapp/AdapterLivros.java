81638724f11b5357de2b2c99277ca346f92d5803
lndsilva <lndsilva@hotmail.com>
2026-05-28 20:24:21 -0300

    lista livros

--- app/src/main/java/br/com/etecia/myapp/AdapterLivros.java
+++ app/src/main/java/br/com/etecia/myapp/AdapterLivros.java
@@ -2,9 +2,13 @@ package br.com.etecia.myapp;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class AdapterLivros extends RecyclerView.Adapter<AdapterLivros.ViewHolder> {
 
@@ -24,10 +28,19 @@ public class AdapterLivros extends RecyclerView.Adapter<AdapterLivros.ViewHolder
         return 0;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        MaterialCardView modeloCardLivros;
+        ImageView modeloImgLivros;
+        TextView modeloTituloLivro;
+
 
         public ViewHolder(@NonNull View itemView) {
+
             super(itemView);
+            modeloTituloLivro = itemView.findViewById(R.id.modeloTituloLivro);
+            modeloImgLivros = itemView.findViewById(R.id.modeloImgLivros);
+            modeloCardLivros = itemView.findViewById(R.id.modeloCardLivros);
         }
     }
 }
