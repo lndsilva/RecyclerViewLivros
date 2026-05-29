0406847982158d1b8629acf778b23bb6fab4b271
lndsilva <lndsilva@hotmail.com>
2026-05-28 21:29:54 -0300

    lista livros

--- app/src/main/java/br/com/etecia/myapp/AdapterLivros.java
+++ app/src/main/java/br/com/etecia/myapp/AdapterLivros.java
@@ -1,5 +1,7 @@
 package br.com.etecia.myapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -10,22 +12,39 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.card.MaterialCardView;
 
+import java.util.List;
+
 public class AdapterLivros extends RecyclerView.Adapter<AdapterLivros.ViewHolder> {
+
+    private Context context;
+    private List<Livros> lstLivros;
+
+    public AdapterLivros(Context context, List<Livros> lstLivros) {
+        this.context = context;
+        this.lstLivros = lstLivros;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view;
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        view = inflater.inflate(R.layout.modelo_livros,parent,false);
+
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.modeloTituloLivro.setText(lstLivros.get(position).getTitulo());
+        holder.modeloImgLivros.setImageResource(lstLivros.get(position).getImgLivro());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return lstLivros.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
